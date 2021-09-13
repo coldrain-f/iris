260afe0ddbb5fae4a2bbdf43ed9c4c80b58d5cc0
coldrain-f <intmain__@naver.com>
2021-09-13 11:45:39 +0900

    Initial commit

---
+++ src/main/java/edu/coldrain/iris/IrisApplication.java
@@ -0,0 +1,13 @@
+package edu.coldrain.iris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrisApplication.class, args);
+    }
+
+}
