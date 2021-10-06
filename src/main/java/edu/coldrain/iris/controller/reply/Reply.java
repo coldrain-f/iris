07825652182f3a020c9c6b41e453a2de6f96355d
coldrain-f <intmain__@naver.com>
2021-10-06 14:56:41 +0900

    덧글 수정, 삭제 모달창 화면 구현 완료

---
+++ src/main/java/edu/coldrain/iris/controller/reply/Reply.java
@@ -0,0 +1,14 @@
+package edu.coldrain.iris.controller.reply;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Reply {
+
+
+    private String contents;
+}
---
+++ src/main/java/edu/coldrain/iris/controller/reply/ReplyController.java
@@ -0,0 +1,14 @@
+package edu.coldrain.iris.controller.reply;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ReplyController {
+
+    //CREATE
+
+    public void create() {
+
+    }
+
+}
